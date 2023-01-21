11a5181931b6224dff7378eb80a5b809fe60ceb5
Kristof-De-Bock <debockkristof@gmail.com>
2023-01-21 08:30:02 +0100

    LW1 - Initial front-end commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('learning-wizard app is running!');
+    expect(page.getTitleText()).toEqual('learning-wizard-front app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learning-wizard'`, () => {
+  it(`should have as title 'learning-wizard-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('learning-wizard');
+    expect(app.title).toEqual('learning-wizard-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('learning-wizard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('learning-wizard-front app is running!');
   });
 });
